11ad34ee22ea0918ba6647d93b3bba1bf259af4a
Chegur <chegurovnick@mail.ru>
2026-02-25 09:43:55 +0400

    Update lab-1.1.py
    Renamed const. And made paths relative

--- lab-1.1.py
+++ lab-1.1.py
@@ -1,24 +1,24 @@
-def create_atbash_mapping():
+def create_atbash_mapping() -> dict:
     """Создаёт словарь подстановки для шифра Атбаш (русский алфавит)"""
-    alphabet = 'абвгде жзийклмнопрстуфхцчшщъыьэюя'
-    reversed_alphabet = alphabet[::-1]
+    ALPHABET = 'абвгде жзийклмнопрстуфхцчшщъыьэюя'
+    REVERSED_ALPHABET = ALPHABET[::-1]
     
     mapping = {}
-    for orig, subst in zip(alphabet, reversed_alphabet):
+    for orig, subst in zip(ALPHABET, REVERSED_ALPHABET):
         mapping[orig] = subst
         mapping[orig.upper()] = subst.upper()
     
     return mapping
 
-def encrypt_atbash(text, mapping):
+def encrypt_atbash(text, mapping) -> str:
     """Шифрует текст с помощью шифра Атбаш"""
     return ''.join(mapping.get(char, char) for char in text)
 
-def decrypt_atbash(text, mapping):
+def decrypt_atbash(text, mapping) -> str:
     """Расшифровывает текст (для Атбаш процесс идентичен шифрованию)"""
     return encrypt_atbash(text, mapping)
 
-def process_file(input_file, output_file, mode='encrypt'):
+def process_file(input_file, output_file, mode='encrypt') -> None:
     """Обрабатывает файл: шифрует или расшифровывает"""
     mapping = create_atbash_mapping()
     
@@ -43,6 +43,6 @@ def process_file(input_file, output_file, mode='encrypt'):
 
 if __name__ == '__main__':
     # Пример использования
-    #process_file('D:\Учеба\Лабы\ОИБ\lab1\lab-1.1_text.txt', 'D:\Учеба\Лабы\ОИБ\lab1\encrypted.txt', mode='encrypt')
+    #process_file('lab-1.1_text.txt', 'encrypted.txt', mode='encrypt')
     # Для расшифровки:
-    process_file('D:\Учеба\Лабы\ОИБ\lab1\encrypted.txt', 'D:\Учеба\Лабы\ОИБ\lab1\decrypted.txt', mode='decrypt')
+    process_file('encrypted.txt', 'decrypted.txt', mode='decrypt')
